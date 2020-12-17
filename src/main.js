098c8fd15159fe208aa83594d5541544b969237b
Tristan Culp <tculp@mit.edu>
2020-12-17 17:54:05 -0500

    Reinstalled node modules, added firebase authentication support

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueCookie from 'vue-cookie'
+import firebase from 'firebase/app'
 
 import App from './App.vue'
 import router from './router'
@@ -8,6 +9,19 @@ Vue.use(VueCookie);
 
 export const eventBus = new Vue();
 Vue.config.productionTip = false
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDQxEAXDmPsM2j-hLJuniRSfMn3HqtpRcI",
+  authDomain: "bthc-volunteer-service.firebaseapp.com",
+  databaseURL: "https://bthc-volunteer-service.firebaseio.com",
+  projectId: "bthc-volunteer-service",
+  storageBucket: "bthc-volunteer-service.appspot.com",
+  messagingSenderId: "610410716655",
+  appId: "1:610410716655:web:b2b07177666913b8482a1a",
+  measurementId: "G-W3P3FTGZC1"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,

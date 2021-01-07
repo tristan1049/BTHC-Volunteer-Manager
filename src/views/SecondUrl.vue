b1d5bdb704b7c964cbdcef56a027085e5d611019
Tristan Culp <tculp@mit.edu>
2021-01-07 16:10:37 -0500

    Implement firebase functions in components themselves, fix authentication

--- src/views/SecondUrl.vue
+++ src/views/SecondUrl.vue
@@ -3,6 +3,9 @@
     <div id="thing">
         <h3>Is new user: {{ newUser }}</h3>
         <h3>User data: {{ user_data }}</h3>
+        <router-link to='/third'>
+            <button>Take me to Third Page!</button>
+        </router-link>
     </div>
   </div>
 </template>
@@ -11,59 +14,74 @@
 import firebase from "firebase/app";
 import 'firebase/auth';
 import 'firebase/database';
-import axios from "axios";
 
 export default {
   name: 'second',
   components: {
   },
-  created() {
-    const user = firebase.auth().currentUser;
-    if (user) {
-        // User is signed in.
-        if (this.newUser){
-            this.createUser(user);
+  mounted() {
+    let self = this;
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            // User is signed in, create user if new
+            if (self.newUser){
+                self.createUser(user);
+            }
+            // Set user_data and check for updates to it
+            self.getUser(user.uid);
+            self.initLoop()
         }
-        // Set user_data
-        this.getUser(user.uid);
-    }
-    else {
-        // No user is signed in.
-        console.log("Error: No user is signed in!");
-    }
+        else {
+            // No user is signed in.
+            console.log("Error: No user is signed in!");
+        }
+    });
   },
   data() {
       return {
           newUser: this.$cookie.get('isNewUser'),
-          user_data: null,
-          loc_data: null
+          user_data: null
       }
   },
   methods: {
-      createUser: function(user) {
-          axios
-          .post(`/api/user/create`, { user: user})
-          .then(() => {
-              // axios request successful
-          })
-          .catch((err) => {
-              // axios request failed, do nothing
-              console.log(err);
-          })
+      createUser: async function(user) {
+        await firebase.database().ref('user-data/' + user.uid).set({
+            displayName: user.displayName,
+            email: user.email,
+            uid: user.uid,
+            role: "volunteer",
+            site: null,
+            skills: null
+        }, function(error) {
+            if (error) {
+                // The write failed...
+                console.log("Error: Could not add user to database!");
+                console.log(error);
+                return false;
+            }
+            else {
+                // Data saved successfully!
+                return true;
+            }
+        });
       },
-      getUser: function(user_uid) {
-          axios
-          .get(`/api/user/${user_uid}`)
-          .then((res) => {
-              // axios request successful, set user_data to received data
-              this.user_data = res;
-          })
-          .catch((err) => {
-              //axios request failed, do nothing
-              console.log(err);
-          })
+      getUser: async function(user_uid) {
+        var data = await firebase.database().ref('user-data/' + user_uid).once('value').then((snapshot) => {
+            return snapshot.val();
+        });
+        this.user_data = data;
+      },
+      initLoop: async function(user_id) {
+        let self = this;
+        await firebase.database().ref('user-data/' + user_id).on('value', function(snapshot) {
+            let data = [];
+            snapshot.forEach(function(childSnapshot) {
+                data.push(childSnapshot.val());
+                // Fill the local data property with Firebase data
+                self.user_data = data;
+    });
+        });
       }
-
   }
 }
 </script>
--- src/views/ThirdUrl.vue
+++ src/views/ThirdUrl.vue
@@ -3,8 +3,8 @@
     <div id="thing">
         <h3>This is the third page! Wow</h3>
         <h3>User data: {{ user_data }}</h3>
-        <router-link to="/third">
-            <button>Take me to third page!</button>
+        <router-link to='/second'>
+            <button>Okay back to second page</button>
         </router-link>
     </div>
   </div>
@@ -14,33 +14,46 @@
 import firebase from "firebase/app";
 import 'firebase/auth';
 import 'firebase/database';
-import axios from "axios";
 
 export default {
   name: 'third',
   components: {
   },
-  created() {
-    // Get current user credentials
-    const user = firebase.auth().currentUser;
-
-    // Use axios to get current user data
-    axios
-    .post(`/api/user/${user.uid}`)
-    .then((res) => {
-        // axios request successful, set user_data to received data
-        this.user_data = res.data;
-    })
-    .catch((err) => {
-        //axios request failed, do nothing
-        console.log(err);
-    })
+  mounted() {
+    let self = this;
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+          self.getUser(user.uid);
+        }
+        else {
+            // No user is signed in.
+            console.log("Error: No user is signed in!");
+        }
+    });
+  },
+  methods: {
+      getUser: async function(user_uid) {
+        var data = await firebase.database().ref('user-data/' + user_uid).once('value').then((snapshot) => {
+            return snapshot.val();
+        });
+        this.user_data = data;
+      }
   },
   data() {
       return {
           user_data: null
       }
   }
-
 }
 </script>
+
+<style>
+#thing {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 200px;
+}
+</style>
